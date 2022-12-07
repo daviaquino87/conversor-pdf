6763147450fdd2e4faa1d41b795a686c4a7ee016
daviaquino87 <davi26031@gmail.com>
2022-12-07 17:48:04 -0300

    feat: gerando html com a tabela

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const Reader = require('./Reader')
 const Processor = require("./Processor")
 const Table = require("./Table")
+const HtmlParser = require("./HtmlParser")
 
 const reader = new Reader()
 
 async function main(){
   const dados = await reader.Read("./file.csv")
   const processorData = Processor.Process(dados)
-
   const usuarios = new Table(processorData);
+
+  var html = await HtmlParser.Parse(usuarios);
   
 
-  console.log(usuarios.HeadersCount)
+  console.log(html)
 }
 
 main()
