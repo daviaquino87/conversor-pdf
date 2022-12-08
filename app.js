54dedf4e3022b997ff35f1206030d86a735abec5
daviaquino87 <davi26031@gmail.com>
2022-12-08 14:37:05 -0300

    feat: end

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const Reader = require('./Reader')
 const Processor = require("./Processor")
 const Table = require("./Table")
 const HtmlParser = require("./HtmlParser")
-const Writer = require("./Writer")
+// const Writer = require("./Writer")
 const WriterPDF = require("./WriterPDF")
 
 const reader = new Reader()
-const writer = new Writer()
+// const writer = new Writer()
 
 async function main(){
   const dados = await reader.Read("./file.csv")
@@ -14,7 +14,7 @@ async function main(){
   const usuarios = new Table(processorData);
 
   var html = await HtmlParser.Parse(usuarios);
-  // writer.Write( Date.now() +".html",html) gera o arquivo html
+  // writer.Write( Date.now() +".html",html) 
   WriterPDF.WriterPdf( Date.now() +".PDF",html)
 
 }
